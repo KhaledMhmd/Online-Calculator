07726bf5fff0e8c14bbb553d3e5dc84223bc9fdc
Khaled Mohamed <khaledmhmd174@gmail.com>
2020-10-31 19:40:31 +0200

    updating my js codes

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ var equalButton = document.getElementById('equals');
 var clearAll = document.getElementById('clear');
 var del = document.getElementById('del');
 var output = document.getElementById('outputDisplay');
-var seven = document.getElementById('seven');
-var eight = document.getElementById('eight');
-var nine = document.getElementById('nine');
-var six = document.getElementById('six');
-var five = document.getElementById('five');
-var four = document.getElementById('four');
-var three = document.getElementById('three');
-var two = document.getElementById('two');
-var one = document.getElementById('one');
-var zero = document.getElementById('zero');
-var point = document.getElementById('point');
 
-for (let i=0; i<numberButtons.length; i++){
+  for (let i=0; i<numberButtons.length; i++){
 numberButtons[i].addEventListener("click", display);
 function display(){
-  output.innerHTML = numberButtons[i].value;
+      output.innerHTML = output.innerHTML + numberButtons[i].value ;
+}};
+
+for (let i=0; i<mathOpButtons.length; i++){
+mathOpButtons[i].addEventListener("click", display);
+function display(){
+  output.innerHTML = output.innerHTML + mathOpButtons[i].value ;
 }};
 
 clearAll.onclick = function clear(){
   output.innerHTML = "";
 };
+
+
+equalButton.onclick = function compute(){
+  output.innerHTML = eval(output.innerHTML);
+}
+
+del.onclick = function remove(){
+    output.innerHTML = output.innerHTML.substring(0, output.innerHTML.length -1);
+}
